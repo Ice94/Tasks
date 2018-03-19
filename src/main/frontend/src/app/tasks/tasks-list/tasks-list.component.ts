bc222388ea467cafe96bd1381ab8ca831ad5a5ff
Ice94 <mhbratek@gmail.com>
2018-03-20 00:05:20 +0100

    Retriving data from Spring boot endpoint using Angular 4.

--- src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Task} from "../task.model";
+import {TaskService} from "../task.service";
 
 @Component({
     selector: 'app-tasks-list',
@@ -11,13 +12,16 @@ export class TasksListComponent implements OnInit {
 
     tasks: Task[] = [];
 
-    constructor() {
+    constructor(private taskService: TaskService) {
     }
 
     ngOnInit() {
-        this.tasks.push(new Task(1, "Task 1", true, "07/07/17"));
-        this.tasks.push(new Task(2, "Task 2", true, "08/07/17"));
-        this.tasks.push(new Task(3, "Task 3", true, "09/07/17"));
+        return this.taskService.getTasks().subscribe(
+            (tasks: any[]) => {
+                this.tasks = tasks;
+            },
+            (error) => console.log(error)
+        );
     }
 
     getDueDateLabel(task: Task) {
